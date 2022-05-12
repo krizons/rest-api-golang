031272c1aa8f776bff164920e8b2324bb6bfde9a
krizons <77729624+krizons@users.noreply.github.com>
2022-05-12 19:17:50 +0300

    add mysql

---
+++ internal/user_db/mysql/db_user.go
@@ -0,0 +1,150 @@
+package mysql_user
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/krizons/rest-api-golang/internal/model"
+)
+
+func (db *Db) GetAll() ([]model.User, error) {
+	var users []model.User
+	results, err := db.Db.Query("SELECT name,age,country FROM users")
+	if err != nil {
+		return nil, err
+	}
+	for results.Next() {
+		var user model.User
+
+		err = results.Scan(&user.Name, &user.Age, &user.Country)
+		users = append(users, user)
+		if err != nil {
+			return nil, err
+		}
+
+	}
+
+	return users, nil
+}
+
+func (db *Db) Create(user model.User) error {
+	insert, err := db.Db.Query("INSERT INTO users( name,age,country ) VALUES ( ?, ?, ? )", user.Name, user.Age, user.Country)
+	if err != nil {
+		return err
+	}
+	insert.Close()
+	return nil
+	//db.Db.Create(&user)
+
+}
+func (db *Db) Filter(user_filtr model.User) ([]model.User, error) {
+	var users []model.User
+	var qtext string = ""
+	if user_filtr.Age != 0 {
+
+		qtext = fmt.Sprintf("SELECT name,age,country FROM users WHERE %s = %d;", "age", user_filtr.Age)
+	} else if user_filtr.Country != "" {
+
+		qtext = fmt.Sprintf("SELECT name,age,country FROM users WHERE %s = \"%s\";", "country", user_filtr.Country)
+	} else if user_filtr.Name != "" {
+
+		qtext = fmt.Sprintf("SELECT name,age,country FROM users WHERE %s = \"%s\";", "name", user_filtr.Name)
+	}
+
+	//results, err := db.Db.Query("SELECT name,age,country FROM users WHERE ? = ?;", colum, value)
+	results, err := db.Db.Query(qtext)
+	if err != nil {
+		return nil, err
+	}
+	for results.Next() {
+		var user model.User
+
+		err = results.Scan(&user.Name, &user.Age, &user.Country)
+		users = append(users, user)
+		if err != nil {
+			return nil, err
+		}
+
+	}
+
+	return users, nil
+
+}
+func (db *Db) Range(age_start int, end_age int) ([]model.User, error) {
+	var users []model.User
+	results, err := db.Db.Query("SELECT name,age,country FROM users WHERE age >= ? and age <= ?", age_start, end_age)
+
+	if err != nil {
+		return nil, err
+	}
+	for results.Next() {
+		var user model.User
+
+		err = results.Scan(&user.Name, &user.Age, &user.Country)
+		users = append(users, user)
+		if err != nil {
+			return nil, err
+		}
+
+	}
+
+	return users, nil
+
+}
+func (db *Db) Order(field string, sorting string) ([]model.User, error) {
+	//ORDER BY city DESC
+	var users []model.User
+	qtext := fmt.Sprintf("SELECT name,age,country FROM users ORDER BY %s %s", field, sorting)
+	results, err := db.Db.Query(qtext)
+
+	if err != nil {
+		return nil, err
+	}
+	for results.Next() {
+		var user model.User
+
+		err = results.Scan(&user.Name, &user.Age, &user.Country)
+		users = append(users, user)
+		if err != nil {
+			return nil, err
+		}
+
+	}
+
+	return users, nil
+	/*res := db.Db.Order(field + " " + sorting).Find(&users)
+	return users, res.Error*/
+}
+func (db *Db) TextSearch(search string) ([]model.User, error) {
+	var users []model.User
+	var out_user []model.User
+	results, err := db.Db.Query("SELECT name,age,country FROM users")
+
+	if err != nil {
+		return nil, err
+	}
+	for results.Next() {
+		var user model.User
+
+		err = results.Scan(&user.Name, &user.Age, &user.Country)
+		users = append(users, user)
+		if err != nil {
+			return nil, err
+		}
+
+	}
+
+	for _, person := range users {
+		if strings.Contains(person.Name, search) {
+			out_user = append(out_user, person)
+		}
+	}
+	return out_user, nil
+}
+func (db *Db) CreateTable() {
+	insert, _ := db.Db.Query("CREATE TABLE `rest-api`.`users` ( `id` INT NOT NULL AUTO_INCREMENT , `name` TEXT NOT NULL , `age` INT NOT NULL , `country` TEXT NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;")
+	if insert != nil {
+		insert.Close()
+	}
+
+}
---
+++ internal/user_db/mysql/db_user_test.go
@@ -0,0 +1,92 @@
+package mysql_user_test
+
+import (
+	"testing"
+
+	"github.com/krizons/rest-api-golang/internal/model"
+	sql_user "github.com/krizons/rest-api-golang/internal/user_db/mysql"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateGetSelect(t *testing.T) {
+	assert := assert.New(t)
+	test_db, err := sql_user.New("root@tcp(127.0.0.1:3306)/rest-api")
+	assert.NoError(err)
+	test_db.Create(model.User{Name: "Margie Ellison", Age: 18, Country: "UK"})
+	test_db.Create(model.User{Name: "Gordon Felix", Age: 20, Country: "US"})
+	test_db.Create(model.User{Name: "Johnnie Burton", Age: 45, Country: "US"})
+	test_db.Create(model.User{Name: "Lillie Perkins", Age: 18, Country: "AT"})
+	test_db.Create(model.User{Name: "Moses Jarvis", Age: 64, Country: "AT"})
+	test_db.Create(model.User{Name: "Margie Ellison", Age: 18, Country: "UK"})
+	test_db.Create(model.User{Name: "Gordon Felix", Age: 20, Country: "US"})
+	test_db.Create(model.User{Name: "Johnnie Burton", Age: 45, Country: "US"})
+	test_db.Create(model.User{Name: "Lillie Perkins", Age: 18, Country: "AT"})
+	test_db.Create(model.User{Name: "Moses Jarvis", Age: 64, Country: "AT"})
+	users, err := test_db.GetAll()
+	assert.NoError(err)
+	assert.NotEqual(len(users), 0)
+
+}
+func TestFindParam(t *testing.T) {
+	assert := assert.New(t)
+	test_db, err := sql_user.New("root@tcp(127.0.0.1:3306)/rest-api")
+	assert.NoError(err)
+	filter_out, err := test_db.Filter(model.User{Country: "AT"})
+	assert.NoError(err)
+	assert.NotEqual(len(filter_out), 0)
+	filter_out, err = test_db.Filter(model.User{Country: "UK"})
+	assert.NoError(err)
+	assert.NotEqual(len(filter_out), 0)
+	filter_out, err = test_db.Filter(model.User{Age: 18})
+	assert.NoError(err)
+	assert.NotEqual(len(filter_out), 0)
+
+}
+func TestRange(t *testing.T) {
+	assert := assert.New(t)
+	test_db, err := sql_user.New("root@tcp(127.0.0.1:3306)/rest-api")
+	assert.NoError(err)
+	range_out, err := test_db.Range(18, 30)
+	assert.NoError(err)
+	for _, person := range range_out {
+		assert.LessOrEqual(uint8(18), person.Age)
+		assert.GreaterOrEqual(uint8(30), person.Age)
+
+	}
+}
+func TestOrder(t *testing.T) {
+	assert := assert.New(t)
+	test_db, err := sql_user.New("root@tcp(127.0.0.1:3306)/rest-api")
+	assert.NoError(err)
+	range_out, err := test_db.Order("age", "desc")
+	assert.NoError(err)
+	assert.NotEmpty(range_out)
+	var tmp uint8 = 255
+	for _, person := range range_out {
+		assert.GreaterOrEqual(tmp, person.Age)
+		tmp = person.Age
+
+	}
+	range_out, err = test_db.Order("age", "asc")
+	assert.NotEmpty(range_out)
+	assert.NoError(err)
+	tmp = 0
+	for _, person := range range_out {
+		assert.LessOrEqual(tmp, person.Age)
+		tmp = person.Age
+
+	}
+}
+func TestTextFins(t *testing.T) {
+	assert := assert.New(t)
+	test_db, err := sql_user.New("root@tcp(127.0.0.1:3306)/rest-api")
+	assert.NoError(err)
+	range_out, err := test_db.TextSearch("Margie")
+	assert.NoError(err)
+
+	for _, person := range range_out {
+		assert.Contains(person.Name, "Margie")
+
+	}
+
+}
